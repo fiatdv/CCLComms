b81dd489b6b731f1b131cb50ec602d52920849e7
Felipe Gomez <fiatdv>
2022-05-26 03:48:01 -0400

    Initial Commit

---
+++ CCLComms/CCLCommsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCLCommsApp.swift
+//  CCLComms
+//
+//  Created by Felipe on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CCLCommsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
